e5918e8bfbe2017085d3b59587e7c3d66551ba3d
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-08-06 07:51:36 +0100

    updated based on serverless examples

--- handler.py
+++ handler.py
@@ -1,11 +1,16 @@
 import json
+import datetime
+
 
 def endpoint(event, context):
-body = {
-"message": "Hello Serverless training team"
+    current_time = datetime.datetime.now().time()
+    body = {
+        "message": "Hello, the current time is " + str(current_time)
     }
+
     response = {
-"statusCode": 200,
-"body": json.dumps(body)
+        "statusCode": 200,
+        "body": json.dumps(body)
     }
+
     return response
